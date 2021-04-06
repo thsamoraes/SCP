b6e53d2afd8e4b7fc96d44557558eba548fc0ce2
Thaís Moraes <80347884+thsamoraes@users.noreply.github.com>
2021-04-06 12:42:01 -0300

    Create Exercise7.py

---
+++ Chapter4/Exercise7.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+v = np.array([[1.,-1.,1.]])
+print("v = \n",v)
+print("\nv transposto (v^T) = \n",v.T)
+
+P = np.matmul(v,v.T)/np.matmul(v.T,v)
+
+I = np.identity(3)
+
+Q = I - P
+
+print("\nP = \n",P)
+
+print("\nQ = \n",Q)
+
+
+print('\nP*v = \n', np.matmul(P,(v.T)))
+
+print('v^T é autovetor de P com autovalor 9')
+
+
+print('\nQ*v = \n', np.matmul(Q,(v.T)))
+
+print('v^T é autovetor de Q com autovalor -8')
