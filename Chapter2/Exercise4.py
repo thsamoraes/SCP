cc3c38b4559b9e95eff342c9508f6d70210210d6
Thaís Moraes <80347884+thsamoraes@users.noreply.github.com>
2021-04-12 11:09:43 -0300

    Update Exercise4.py

--- Chapter2/Exercise4.py
+++ Chapter2/Exercise4.py
@@ -1,12 +1,8 @@
-# Ex. 4 → Suppose we are trying to check the convergence of a 
-# diverging sequence (here the sequence is defined by the recursive relation 
-# u_n+1 = 2u_n and u_0 = 1.0):
-
-u = 1.0 # you have to use a float here!
-uold = 10.0
+u = 1
+uold = 10
 
 for iteration in range(2000):
-    if abs(u-uold) > 1.e-8:
+    if not abs(u-uold) > 1.e-8:
         print('Convergence')
         break # sequence has converged
     uold = u
@@ -14,6 +10,7 @@ for iteration in range(2000):
 else:
     print('No convergence')
 
-# 1. prints 'Convergence'
-# 2. prints 'No convergence'
-# 3. não mudou ??
+# Quando fazemos a série com números inteiros, podemos computar a quantidade 
+# de números que a memória do computador aguenta, mas quando fazemos com float 
+# ele nos dá infinito e a comparação de infinito dará nan que retorna falso 
+# quando é comparado.
