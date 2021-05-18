5ef5ce2e14019c9be082d728ac6cf76431b1fba2
Thaís Moraes <80347884+thsamoraes@users.noreply.github.com>
2021-05-18 12:53:03 -0300

    Create Exercise1.py

---
+++ Chapter9/Exercise1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def f(x):
+    return 1 / np.sqrt(x)
+   
+lista = []
+
+for i in np.arange(1,200,1):
+    lista.append(f(i))
+
+print(lista)    
+print("A soma da lista é: ",sum(lista))
